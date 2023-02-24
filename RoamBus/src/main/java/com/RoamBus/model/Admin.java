ce399dec6b5c169b04dec11b2840b868244c6cb6
rakesh7063 <rakeshkandar2021@gmail.com>
2023-02-24 18:55:10 +0530

    add vaildation

--- RoamBus/src/main/java/com/RoamBus/model/Admin.java
+++ RoamBus/src/main/java/com/RoamBus/model/Admin.java
@@ -6,7 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -29,10 +31,11 @@ public class Admin {
 	
 	@NotNull(message = "Please enter the username")
 	private String adminUsername;
-	
-	@NotNull(message = "Please Enter the password")
-	@Size(max = 15, min = 8, message = "Your password should be at least of 8 charecters and at most of 15 charecters")
-	private String password;
+
+	@NotBlank(message = "Password cannot be blank")
+	@Size(min = 6, max = 12, message = "Password must be between 6 and 12 characters")
+	@Pattern(regexp =  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{5,15}$",message = "password must contain atleast 1 uppercase, 1 lowercase, 1 special character and 1 digit")
+	private  String password;
 
 	
 	
--- RoamBus/src/main/java/com/RoamBus/model/Bus.java
+++ RoamBus/src/main/java/com/RoamBus/model/Bus.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -57,8 +58,10 @@ public class Bus {
 	private LocalTime arrivalTime;
 		
 	@NotNull(message = "Please enter the total seats")
+	@Size(min = 10, max = 50, message = "Seats Number should be 10 to 50")
 	private int seats;
-	
+
+	@NotNull(message = "Please Enter available seats")
 	private int availableSeats;
 
 	
